dcdb53b1b04254eebd5c16c619c3c7a0c3abac18
Alex Krytskyi <alexk@cirkledin.com>
2018-10-04 22:37:58 -0700

    initial express server with routes files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb://dbAlexDev:S3cur3PWD@ds223653.mlab.com:23653/devconnector'
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+//setting up routes
+app.get('/', (req, res) => res.send('Hello!'));
+
+//User routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 50000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
